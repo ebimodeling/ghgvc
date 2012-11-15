4cbc4a7acedeb7168ab280fca28848d4f72ff0da
Carl Crott <carlcrott@gmail.com>
2012-11-15 12:37:31 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ghgvc::Application.config.secret_token = '85594954acaee4f2dd6512c33f9706dd064352391beaa107240a91c0a2e079b761b8abec178c24bd459e3cf4639497e213742fda7da50e199980be6132b08efd'
