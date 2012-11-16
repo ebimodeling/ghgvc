9ddec716de869e60adf0220219e79c00930893d8
Carl Crott <carlcrott@gmail.com>
2012-11-16 15:18:19 -0600

    added in google maps js ( needs relocated ) and enabled js for bootstrap

---
+++ app/assets/javascripts/google_maps_api.js
@@ -0,0 +1,44 @@
+var geocoder;
+var address;
+var latlng = new google.maps.LatLng(40.44,-95.98);
+var myOptions = {
+  zoom: 4,
+  center: latlng,
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+
+map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+overlay = [];
+
+google.maps.event.addListener(map, "click", function(event) { 
+
+  if(overlay.length > 0) {        
+    overlay[0].setMap(null);
+    overlay.length = 0;
+  }
+  var radius = $('radius').value;
+  var lat = event.latLng.lat();
+  var lon = event.latLng.lng();
+  <%= remote_function( :url => { :action => "show_sites"}, :with => "'lat=' + lat + '&lng=' + lon + '&radius=' + radius"  ) %>;
+
+  var latOffset = radius/(69.1);
+  var lonOffset = radius/(53.0);
+  var paths = [new google.maps.LatLng(lat + latOffset, lon + lonOffset),
+             new google.maps.LatLng(lat - latOffset, lon + lonOffset),
+             new google.maps.LatLng(lat - latOffset, lon - lonOffset),
+             new google.maps.LatLng(lat + latOffset, lon - lonOffset),
+             new google.maps.LatLng(lat + latOffset, lon + lonOffset)];
+  overlay.push( new google.maps.Polygon({
+    paths: paths,
+    strokeColor: "#ff0000",
+    strokeWeight: 0,
+    strokeOpacity: 1,
+    fillColor: "#ff0000",
+    fillOpacity: 0.2,
+    clickable: false,
+    map: map
+  }))
+});
+
+
