5060806b92dea1dfb6b0f94c46e03727517fd78c
Carl Crott <carlcrott@gmail.com>
2012-11-17 22:42:50 -0600

    google maps api / js correctly draws selecting red square

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,4 +8,19 @@ describe "Home page" do
   end
 end
 
+#describe "Home page" do
+#  before { visit root_path } 
 
+#  it "should have the h1 'Sample App'" do
+#    page.should have_selector('h1', text: 'Sample App')
+#  end
+
+#  it "should have the base title" do
+#    page.should have_selector('title',
+#                      text: "Ruby on Rails Tutorial Sample App")
+#  end
+
+#  it "should not have a custom page title" do
+#    page.should_not have_selector('title', text: '| Home')
+#  end
+#end
