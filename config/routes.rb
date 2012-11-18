6c6f2576c6e21e332874fb665fe626ee97bf8795
Carl Crott <carlcrott@gmail.com>
2012-11-18 14:42:53 -0600

    set static_home as default landing page

--- config/routes.rb
+++ config/routes.rb
@@ -63,7 +63,7 @@ Ghgvc::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
