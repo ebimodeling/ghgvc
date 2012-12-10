cd4aecb554e8e026638650755e1d4e81999fab41
Carl Crott <carlcrott@gmail.com>
2012-12-10 12:18:41 -0500

    map_type_selector buttons correctly control map swapping

--- app/assets/javascripts/workflows.js
+++ app/assets/javascripts/workflows.js
@@ -1,6 +1,9 @@
 $(function(){
 
-    console.log("yar");
-    
+//    $('.map_type_selector').on('click', function () {
+//      $(document).find('.map_type_selector').removeClass('active');
+//      $(this).addClass('active');
+//      initalize_google_maps();
+//    });
 
 })
