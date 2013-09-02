9188c13e14de5f4863c0e3d4e25f6211789be8a4
Carl Crott <carlcrott@gmail.com>
2013-09-02 08:01:50 -0700

    temp commit -- working on name indexing

--- app/assets/javascripts/workflows.js
+++ app/assets/javascripts/workflows.js
@@ -330,7 +330,7 @@ function show_csep_groups_for_ecosystem( biome_type ,ecosystem_name ) {
     case"temperate forest": case"boreal forest": case"tropical peat forest": case"tropical forest":
       $('#natural_fire').show();
       break;
-   case"corn": case"sugarcane": 
+   case"US corn": case"BR sugarcane": 
       $('#management_related').show();
       $('#fossil_fuel').show();
       break;
@@ -505,6 +505,17 @@ $(document).ready(function() {
     );
   });
   
+  
+    
+    
+//    -0.0348
+//US corn:
+//first * second = third 
+//4th = 3rd * -0.0348
+    
+
+  
+  
   $('#run_ghgvc_calculator').on('click' ,function() {
   
     // Check to see that we've got at least one "checked" input
@@ -512,6 +523,10 @@ $(document).ready(function() {
       alert("Please check one or more ecosystems");  
       return;
     };
+    
+    // when we're missing values, the output will be the same regardless of what checkboxes are checked.
+//    $('#biogeochemical').is(':checked')
+//    $('#biophysical').is(':checked')
     
     // deactivate page with lightbox overlay
     $('#toggle_ghgvcR_processing_popup').trigger("click");
